8c63b91bf45bfef0a718b69258769fe06ea7cd52
Mhmd.S <mhmdslmn20@gmail.com>
2021-12-09 16:33:20 +0200

    Added functionality to the button

--- srcipt.js
+++ srcipt.js
@@ -2,4 +2,9 @@ function printStat(){
     const firstDiv = document.createElement('div');
     const btn = document.createElement('button');
     
+    firstDiv.appendChild(btn);
+    
+    btn.innerHTML = "Click me";
+
+    btn.onclick(console.log("HI"));
 }
